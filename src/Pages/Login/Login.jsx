b10ae652a5a7ce4324908d2552b746ad4ee3b73e
Ashik465 <helloashikul@gmail.com>
2023-05-11 23:15:22 +0600

    log in signup done

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom';
+import logimg from '../../assets/images/login/login.svg'
+
+
+const Login = () => {
+
+
+const handleSignIn = (e) => {
+    e.preventDefault();
+}
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-1/2 lg:pr-20">
+         <img src={logimg} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+          <h1 className="text-3xl text-center font-bold">Login </h1>
+           <form onSubmit={handleSignIn} >
+           <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                name='email'
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                name='password'
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+             
+              <input className="btn bg-orange-500 border-none" type="submit" value="Log-In" />
+            </div>
+
+             <p className='my-4'>new to car doctor <Link className=' text-orange-500' to='/signup'> Sign-up</Link>   </p>
+           </form>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,9 @@ import {
   } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../Pages/Home/Home/Home";
+import LogInLayout from "../layout/LogInLayout";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/sign-up/SignUp";
 
   const router = createBrowserRouter([
     {
@@ -15,6 +18,30 @@ import Home from "../Pages/Home/Home/Home";
           path: "/",
           element: <Home />,
         },
+      ],
+    },
+    {
+      path: "/login",
+      element:<LogInLayout></LogInLayout>,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+       
+      ],
+    },
+    {
+      path: "/signup",
+      element:<LogInLayout></LogInLayout>,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/signup",
+          element: <SignUp></SignUp>,
+        },
+       
       ],
     },
   ]);
