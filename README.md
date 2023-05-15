6333b4bfa4d82d0713ced085cd06aecf0a92bd7b
Ashik465 <helloashikul@gmail.com>
2023-05-15 06:56:16 +0600

    scrollrestoration done ...scroll to top

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # car-doctor-client-react
 # car-doctor-client-react
-[car-doctor-website](https://car-doctor-3fa93.web.app/)
+# [car-doctor-website](https://car-doctor-3fa93.web.app/)
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
 import Footer from "../Pages/Shared/Footer/Footer";
 
@@ -8,6 +8,7 @@ const MainLayout = () => {
             <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
+            <ScrollRestoration></ScrollRestoration>
         </div>
     );
 };
